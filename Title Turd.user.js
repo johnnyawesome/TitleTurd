d2da76fef22ec9771324f5f4db1431dad03ab7ed
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2018-10-21 00:17:27 +0200

    Update Title Turd.user.js

--- Title Turd.user.js
+++ Title Turd.user.js
@@ -7,8 +7,8 @@
 // @match        *://*/*
 // @grant        none
 // @icon         https://cdn0.iconfinder.com/data/icons/pixelo/32/skull.png
-//@updateURL     https://github.com/johnnyawesome/TitleTurd/raw/master/Title%20Turd.user.js
-//@downloadURL   https://github.com/johnnyawesome/TitleTurd/raw/master/Title%20Turd.user.js
+// @updateURL    https://github.com/johnnyawesome/TitleTurd/raw/master/Title%20Turd.user.js
+// @downloadURL  https://github.com/johnnyawesome/TitleTurd/raw/master/Title%20Turd.user.js
 // ==/UserScript==
 
 //lalala
