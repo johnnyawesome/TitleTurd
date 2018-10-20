ab3e90c0a534c3082fd5c4b3bc1290f727fc7ce4
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2018-10-21 00:05:00 +0200

    Update Title Turd.user.js

--- Title Turd.user.js
+++ Title Turd.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         Title Turd
 // @namespace    https://github.com/johnnyawesome
-// @version      1.1
+// @version      1.0
 // @description  Adds a little PooP to the title of your Tab!
 // @author       JohnnyAwesome
 // @match        *://*/*
 // @grant        none
 // @icon         https://cdn0.iconfinder.com/data/icons/pixelo/32/skull.png
-// @updateURL    https://github.com/johnnyawesome/TitleTurd/edit/master/Title%20Turd.user.js
-// @downloadURL  https://github.com/johnnyawesome/TitleTurd/edit/master/Title%20Turd.user.js
+//@updateURL     https://github.com/johnnyawesome/TitleTurd/raw/master/Title%20Turd.user.js
+//@downloadURL   https://github.com/johnnyawesome/TitleTurd/raw/master/Title%20Turd.user.js
 // ==/UserScript==
 
-//lalala
 (function() {
     'use strict';
 
